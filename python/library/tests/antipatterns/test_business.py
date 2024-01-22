080769222b42806a69c3a2609ba46fe82cb0cf01
Krzysztof Stronka <kstronka@apptension.com>
2024-01-22 15:08:42 +0100

    refactor

--- python/library/tests/antipatterns/test_business.py
+++ python/library/tests/antipatterns/test_business.py
@@ -1,7 +1,9 @@
+import datetime as dt
 import pytest
 
 from src.library.domain.business import LibraryBusiness
 from src.library.domain.models import Book, Account
+from src.library.services import notifications
 
 
 class TestLibrary:
@@ -22,3 +24,17 @@ class TestLibrary:
 
         with pytest.raises(Exception):
             business.rent(to=other)
+
+    def test_overdue(self, book, account, mocker):
+        business = LibraryBusiness()
+        mocker.patch('src.library.domain.policies.complex_rental_period_calculation', return_value=10)
+        business.rent(book, to=account)
+        assert book.return_due_date == dt.date.today() + dt.timedelta(days=10)
+
+    def test_notification(self, book, account, mocker):
+        business = LibraryBusiness()
+        business.rent(book, to=account)
+        book.return_due_date = dt.date.fromisoformat('1900-01-01')
+        spy = mocker.spy(notifications, name='external_api_call')
+        business.send_overdue_notification(book)
+        spy.assert_called_once()
