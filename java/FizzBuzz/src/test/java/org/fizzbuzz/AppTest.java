b7f6fc94f7bfc50a6591ceb9013cf1514c2dff27
Krzysztof Stronka <kstronka@apptension.com>
2024-01-24 22:27:41 +0100

    Update java code; update presentation

--- java/FizzBuzz/src/test/java/org/fizzbuzz/AppTest.java
+++ java/FizzBuzz/src/test/java/org/fizzbuzz/AppTest.java
@@ -1,46 +1,50 @@
 package org.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+
 class AppTest {
+    public App klass;
+
+    @BeforeEach
+    void initClass() {
+        klass = new App();
+    }
+
+    @AfterEach
+    void resetClass() {
+        klass = null;
+    }
+
     @Test 
     void getMessageCaseOneThenReturnValue() {
-        App klass = new App();
-
         String expected = "1";
         String actual = klass.getMessage(1);
-
         assertEquals(expected, actual);
     }
 
     @Test 
     void getMessageCaseThreeThenReturnFizz() {
-        App klass = new App();
-
         String expected = "Fizz";
         String actual = klass.getMessage(3);
-
         assertEquals(expected, actual);
     }
 
     @Test 
     void getMessageCaseFiveThenReturnBuzz() {
-        App klass = new App();
-
         String expected = "Buzz";
         String actual = klass.getMessage(5);
-
         assertEquals(expected, actual);
     }
 
     @Test 
     void getMessageCaseFifteenThenReturnFizzBuzz () {
-        App klass = new App();
-
         String expected = "FizzBuzz";
         String actual = klass.getMessage(15);
-
         assertEquals(expected, actual);
     }
 }
